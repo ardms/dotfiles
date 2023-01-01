b9b5c2a7b951000bfcbb75a5ee6969649d380519
ardms <aris@ardmsLaptop.gladstoneLan>
2023-01-01 15:18:32 +0000

    neovim new verion and config from https://www.youtube.com/watch?v=stqUbv-5u2s

--- .config/nvim/lua/keys.lua
+++
@@ -1,8 +0,0 @@
-local keymap = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
-
-vim.g.mapleader = " "
-
--- Stay in indent mode
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", ">gv", opts)
--- .config/nvim/test.py
+++ .config/nvim/test.py
@@ -1,10 +1,2 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
-df = pd.DataFrame()
-
-for i in range(0,100):
-    df.loc[i,'test'] = i**2
-    print(i)
-
-plt.plot(df)
